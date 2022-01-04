60d33be4152eca3684b23768c8a7021dcd12180e
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-01-05 01:07:37 +0530

    POZO Class that stores all constants

--- Constant.java
+++ Constant.java
@@ -1,9 +1,10 @@
 package com;
 
 public class Constant {
-    final static int EXIT = 13;
-    final static int DOCTOREXIT = 5;
-    final static int PATIENTEXIT = 9;
-    final static int APPOINTMENTEXIT = 5;
+    final static int MAIN_MENU_EXIT = 4;
+    final static int SELECTION_MENU_EXIT = 5;
+    final static int DOCTOR_EXIT = 5;
+    final static int PATIENT_EXIT = 9;
+    final static int APPOINTMENT_EXIT = 5;
 
 }
