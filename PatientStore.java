4610ed725a5d51e88480775aa711b1c6d3467361
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-01-05 01:14:37 +0530

    Patient Repository Class containing all Patient Methos and Sets

--- PatientStore.java
+++ PatientStore.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class PatientStore {
     private static PatientStore instance;
-    private Set<Patient> patientList = new HashSet<>();
+    private Set<Patient> patientSet = new HashSet<>();
 
     private PatientStore() {
     }
 
-    public static PatientStore getInstance() {
+    public static synchronized PatientStore getInstance() {
         if (instance == null)
             instance = new PatientStore();
         return instance;
     }
 
     public void add(Patient patient) {
-        patientList.add(patient);
+        patientSet.add(patient);
 
     }
 
     public void remove(Patient patient) {
-        patientList.remove(patient);
+        patientSet.remove(patient);
     }
 
     Patient getPatient(String iD) {
@@ -29,15 +29,15 @@ public class PatientStore {
             if (patientList.get(i).id.equals(iD))
                 return patientList.get(i);
         }*/
-        for( Patient patient: patientList) {
+        for( Patient patient: patientSet) {
             if (patient.id.equals(iD))
                 return patient;
         }
         return null;
     }
 
-    Set<Patient> getPatientList() {
-        return patientList;
+    Set<Patient> getPatientSet() {
+        return patientSet;
 
     }
 
@@ -46,7 +46,7 @@ public class PatientStore {
             if (patientList.get(i).id.equals(iD))
                 return true;
         }*/
-        for( Patient patient: patientList) {
+        for( Patient patient: patientSet) {
             if (patient.id.equals(iD))
                 return true;
         }
