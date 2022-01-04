a54fe40539925e3d4a9dca2ecf87344f3aff2ad0
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-01-05 01:10:50 +0530

    POZO Class that containa all Appointment Class Variables

--- Appointment.java
+++ Appointment.java
@@ -1,6 +1,22 @@
 package com;
 
+import java.util.Objects;
+
 public class Appointment {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Appointment that = (Appointment) o;
+        return Objects.equals(appointmentID, that.appointmentID);
+    }
+
+    @Override
+    public int hashCode() {
+        //return Objects.hash(appointmentID);
+        return 5;
+    }
+
     String appointmentID;
     String patientId;
     String doctorId;
